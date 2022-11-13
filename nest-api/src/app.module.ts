e92ba6c7d475386f248c449b511be999c136f5f6
Pauwul <petrila.paulcosmin@gmail.com>
2022-11-13 23:55:40 +0200

    Add express simplify

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -5,8 +5,10 @@ import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
+console.log("Connection url =>" , process.env.MONGO_URL);
+
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [ProductsModule, MongooseModule.forRoot(process.env.MONGO_URI, {useNewUrlParser: true})],
   // controllers handle incoming requests and send back responses
   controllers: [AppController],
   // providers can be injected into controllers
