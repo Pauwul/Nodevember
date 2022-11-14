142fe4750e6fbc02a00f5044e29597729c60aea1
Paul <paul.petrila@adservio.ro>
2022-11-14 12:32:51 +0200

    Preview InfoVsAC markdown file on the website

---
+++ InfoVsAC/README.md
@@ -0,0 +1,6 @@
+# Info vs AC
+
+<div>
+     <a href="myFile.js" download>Click to Download</a>
+
+</div>
--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -5,10 +5,13 @@ import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
-console.log("Connection url =>" , process.env.MONGO_URL);
+console.log('Connection url =>', process.env.MONGO_URL);
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(process.env.MONGO_URI, {useNewUrlParser: true})],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true }),
+  ],
   // controllers handle incoming requests and send back responses
   controllers: [AppController],
   // providers can be injected into controllers
